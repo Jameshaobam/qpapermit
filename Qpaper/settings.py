cef507685177e84d49d6d44bff494237e2451ba6
Jameshaobam <haobamjamess8@gmail.com>
2021-01-08 12:18:39 +0000

    Removed all stuff

--- Qpaper/settings.py
+++ Qpaper/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r^akl4*hrem2&+j4z2b7a42d$yamv2af!-ygss_e9q&^s7d3i-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['qpaper.pythonanywhere.com']
 
 
 # Application definition
@@ -130,16 +130,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+STATIC_ROOT =  os.path.join(BASE_DIR,'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-cloudinary.config( 
-  cloud_name = "dnrrym85h", 
-  api_key = "536141892299187", 
-  api_secret = "5J5uRCUBMgnwCjk66d4aAbcSsB0" 
+cloudinary.config(
+  cloud_name = "dnrrym85h",
+  api_key = "536141892299187",
+  api_secret = "5J5uRCUBMgnwCjk66d4aAbcSsB0"
 )
